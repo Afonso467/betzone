a1120199d7633703a4faeaa523e48409c702e51c
Afonso467 <afonsodd.alves@gmail.com>
2026-06-22 15:39:37 +0100

    Add GitHub Pages base path

--- novacrates-v2/novacrates-v2/frontend/vite.config.js
+++ novacrates-v2/novacrates-v2/frontend/vite.config.js
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/betzone/', // 👈 adiciona isto
+
   plugins: [react()],
+
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
+
   server: {
     port: 5173,
     proxy: {
